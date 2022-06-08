f64bc0a22cdb7355c4cc9ee7f181c2d9883c2ba4
wdiazplus <dev@wilmerdiaz.com>
2022-06-07 21:06:20 -0500

    Add logs

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import requests
 from prefect import task, Flow
 
-@task
+@task(log_stdout=True)
 def extract():
    response = requests.get("https://jsonplaceholder.typicode.com/posts") 
+   print(f"STATUS: {response.status_code}")
    response = response.json()
    return response
-@task
+
+@task(log_stdout=True)
 def load(response):
    output = response[1]["title"]
    print(output)
 
 
-with Flow("ETL-JSONPlaceHolder API") as flow:
+with Flow("ETL-JsonPlaceHolder API") as flow:
     raw = extract()
     load(raw)
     
