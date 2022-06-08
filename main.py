a686c64b9b08ed62e0d4f2ca8459cc27e1ca2f4d
wdiazplus <dev@wilmerdiaz.com>
2022-06-07 21:57:33 -0500

    Clean code

--- main.py
+++ main.py
@@ -3,20 +3,24 @@ import datetime
 from prefect import task, Flow
 
 @task(log_stdout=True, max_retries=3, retry_delay=datetime.timedelta(seconds=10) )
-def extract():
-   response = requests.get("https://jsonplaceholder.typicode.com/posts") 
-   print(f"STATUS: {response.status_code}")
-   response = response.json()
-   return response
+def extract(id):
+   raw = requests.get(f"https://jsonplaceholder.typicode.com/posts/{id}") 
+   print(f"STATUS: {raw.status_code}")
+   raw = raw.json()
+   return raw 
 
 @task(log_stdout=True, max_retries=3, retry_delay=datetime.timedelta(seconds=10))
-def load(response):
-   output = response[1]["title"]
-   print(output)
+def transform(raw):
+   transform = raw["title"]
+   return transform
 
-
+@task(log_stdout=True, max_retries=3, retry_delay=datetime.timedelta(seconds=10))
+def load(transform):
+    print(str(transform))
+   
 with Flow("ETL-JsonPlaceHolder API") as flow:
-    raw = extract()
-    load(raw)
+    raw = extract(10)
+    transform = transform(raw)
+    load(transform)
     
 flow.run()
