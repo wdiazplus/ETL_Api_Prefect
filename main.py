fb266a3f5077204269697b80fed9321667615508
wdiazplus <dev@wilmerdiaz.com>
2022-06-07 21:14:46 -0500

    Add retries and time for a new task execute

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import requests
+import datetime
 from prefect import task, Flow
 
-@task(log_stdout=True)
+@task(log_stdout=True, max_retries=3, retry_delay=datetime.timedelta(seconds=10) )
 def extract():
    response = requests.get("https://jsonplaceholder.typicode.com/posts") 
    print(f"STATUS: {response.status_code}")
    response = response.json()
    return response
 
-@task(log_stdout=True)
+@task(log_stdout=True, max_retries=3, retry_delay=datetime.timedelta(seconds=10))
 def load(response):
    output = response[1]["title"]
    print(output)
